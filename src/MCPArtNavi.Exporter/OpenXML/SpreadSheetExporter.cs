c18289cb9cd50aae69cd11f7ee9e750cf02a4c9c
a32kita <a32kita@yahoo.co.jp>
2021-04-17 16:15:34 +0900

    Replaced OpenXML Library.

--- src/MCPArtNavi.Exporter/OpenXML/SpreadSheetExporter.cs
+++ src/MCPArtNavi.Exporter/OpenXML/SpreadSheetExporter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Microsoft Corporation Open XML SDK
+// Open XML SDK
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
