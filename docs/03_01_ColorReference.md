ae26283bfaa7604b693977f5252f7d4937033add
a32kita <a32kita@yahoo.co.jp>
2021-04-24 02:07:58 +0900

    wip

--- docs/03_01_ColorReference.md
+++ docs/03_01_ColorReference.md
@@ -1,9 +1,30 @@
 ### 3. リファレンス編
+<!-- 注意: 本ドキュメントは、ColorReference 自動生成ツールにより作成されます -->
+
 その他のリファレンス項目は、[マニュアルのトップページ](README.md#3-リファレンス編) からご覧ください。
 
 #### 3-1. カラー リファレンス
 本アプリケーションでは、各アイテムが次の色と関連付けられています。  
 エクスポート時、およびアプリケーション上での画面表示は、以下の色に基づき、またインポート機能では、次の色に近いアイテムにインポート元データのピクセル情報がマッピングされます。
 
-%TABLE_POSITION%
+| No | Id | Name | Color | Color sample |
+|----|----|------|-------|--------------|
+|1|sand|砂|#EBDFC0|![砂](docs_images/colors/sand.png "砂")|
+|2|white_wool|白色の羊毛|#FAFAFA|![白色の羊毛](docs_images/colors/white_wool.png "白色の羊毛")|
+|3|orange_wool|橙色の羊毛|#F2780D|![橙色の羊毛](docs_images/colors/orange_wool.png "橙色の羊毛")|
+|4|magenta_wool|赤紫色の羊毛|#B83DAF|![赤紫色の羊毛](docs_images/colors/magenta_wool.png "赤紫色の羊毛")|
+|5|light_blue_wool|空色の羊毛|#4CC3ED|![空色の羊毛](docs_images/colors/light_blue_wool.png "空色の羊毛")|
+|6|yellow_wool|黄色の羊毛|#F0C01A|![黄色の羊毛](docs_images/colors/yellow_wool.png "黄色の羊毛")|
+|7|lime_wool|黄緑色の羊毛|#71B914|![黄緑色の羊毛](docs_images/colors/lime_wool.png "黄緑色の羊毛")|
+|8|pink_wool|桜色の羊毛|#FC9EBE|![桜色の羊毛](docs_images/colors/pink_wool.png "桜色の羊毛")|
+|9|gray_wool|灰色の羊毛|#4C5358|![灰色の羊毛](docs_images/colors/gray_wool.png "灰色の羊毛")|
+|10|light_gray_wool|薄灰色の羊毛|#84898C|![薄灰色の羊毛](docs_images/colors/light_gray_wool.png "薄灰色の羊毛")|
+|11|cyan_wool|青緑色の羊毛|#11939A|![青緑色の羊毛](docs_images/colors/cyan_wool.png "青緑色の羊毛")|
+|12|purple_wool|紫色の羊毛|#7E24AB|![紫色の羊毛](docs_images/colors/purple_wool.png "紫色の羊毛")|
+|13|blue_wool|青色の羊毛|#1D489B|![青色の羊毛](docs_images/colors/blue_wool.png "青色の羊毛")|
+|14|brown_wool|茶色の羊毛|#7A4B22|![茶色の羊毛](docs_images/colors/brown_wool.png "茶色の羊毛")|
+|15|green_wool|緑色の羊毛|#5E820C|![緑色の羊毛](docs_images/colors/green_wool.png "緑色の羊毛")|
+|16|red_wool|赤色の羊毛|#AB2222|![赤色の羊毛](docs_images/colors/red_wool.png "赤色の羊毛")|
+|17|black_wool|黒色の羊毛|#050505|![黒色の羊毛](docs_images/colors/black_wool.png "黒色の羊毛")|
+
 
--- src/MCPArtNavi.Tools/MCPArtNavi.Tools.CRMaker/Program.cs
+++ src/MCPArtNavi.Tools/MCPArtNavi.Tools.CRMaker/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -59,6 +60,9 @@ namespace MCPArtNavi.Tools.CRMaker
                 goto start;
             }
 
+
+            // Checks file exists.
+
             var crMdFileName = "03_01_ColorReference.md";
             var crMdFilePath = Path.Combine(docsDir, crMdFileName);
             if (File.Exists(crMdFilePath))
@@ -68,23 +72,96 @@ namespace MCPArtNavi.Tools.CRMaker
                 Console.ReadLine();
             }
 
-            var templateDir = Path.Combine(asmDir, "Assets", "Tootls", "CRMaker");
+
+            // Creates color sample files.
+
+            var cc = new ColorConverter();
+            var enabledItems = MCItemUtils.EnabledItems;
+            var enabledItemSamples = enabledItems.ToDictionary(e => e, e => (Color)cc.ConvertFromString(e.ItemColor));
+
+            var colorsDir = Path.Combine(docsDir, "docs_images", "colors");
+            if (Directory.Exists(colorsDir) == false)
+                Directory.CreateDirectory(colorsDir);
+
+            string _mcItemToSampleImagePath(IMCItem item) => Path.Combine(colorsDir, $"{item.ItemId}.png");
+
+            var imageWidth = 5;
+            var imageHeight = imageWidth;
+            foreach (var kvp in enabledItemSamples)
+            {
+                var sampleImagePath = _mcItemToSampleImagePath(kvp.Key);
+                if (File.Exists(sampleImagePath))
+                {
+                    Console.WriteLine("[INFO] Skipped '{0}' because already file exists.", kvp.Key.ItemId);
+                    continue;
+                }
+
+                using (var bmp = new Bitmap(imageWidth, imageHeight))
+                {
+                    for (var i = 0; i < imageHeight; i++)
+                    {
+                        for (var j = 0; j < imageWidth; j++)
+                            bmp.SetPixel(j, i, kvp.Value);
+                    }
+
+                    bmp.Save(sampleImagePath, ImageFormat.Png);
+                }
+
+                Console.WriteLine("[INFO] Saved color sample for '{0}'.", kvp.Key.ItemId);
+            }
+
+
+            // Loads template.
+
+            var templateDir = Path.Combine(asmDir, "Assets", "Tools", "CRMaker");
             var templateFileBaseName = "03_01_ColorReference";
             var templateFileLang = "ja-JP";
             var templateFileExt = "md";
             var templateFileName = String.Empty;
 
             if (String.IsNullOrEmpty(templateFileLang))
-                templateFileLang = String.Concat(templateFileBaseName + "." + templateFileExt);
+                templateFileName = String.Concat(templateFileBaseName + "." + templateFileExt);
             else
-                templateFileLang = String.Concat(templateFileBaseName + "." + templateFileLang + "." + templateFileExt);
+                templateFileName = String.Concat(templateFileBaseName + "." + templateFileLang + "." + templateFileExt);
 
             var templateData = File.ReadAllText(Path.Combine(templateDir, templateFileName));
 
 
+            // Creates table source.
 
-            var enabledItems = MCItemUtils.EnabledItems;
+            var tableSourceSb = new StringBuilder();
+            tableSourceSb.Append("| No | Id | Name | Color | Color sample |\r\n");
+            tableSourceSb.Append("|----|----|------|-------|--------------|\r\n");
 
+            var scount = 0;
+            foreach (var kvp in enabledItemSamples)
+            {
+                scount++;
+                
+                tableSourceSb.Append("|");
+                tableSourceSb.Append(scount);
+
+                tableSourceSb.Append("|");
+                tableSourceSb.Append(kvp.Key.ItemId);
+                
+                tableSourceSb.Append("|");
+                tableSourceSb.Append(kvp.Key.ItemName);
+
+                tableSourceSb.Append("|");
+                tableSourceSb.Append(kvp.Key.ItemColor);
+
+                tableSourceSb.Append("|");
+                tableSourceSb.Append($"![{kvp.Key.ItemName}](docs_images/colors/{kvp.Key.ItemId}.png \"{kvp.Key.ItemName}\")");
+
+                tableSourceSb.Append("|");
+                tableSourceSb.Append("\r\n");
+            }
+
+            var tableSource = tableSourceSb.ToString();
+
+
+            // Saves document.
+            File.WriteAllText(crMdFilePath, templateData.Replace("%TABLE_POSITION%", tableSource));
         }
     }
 }
